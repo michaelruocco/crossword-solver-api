fc67ac2f5680e5a235d3dbbf157a1961197cd683
Michael Ruocco <michael.ruocco@hotmail.com>
2025-12-08 04:52:45 +0000

    removes unused image processor methods

--- adapters/solver-client/src/main/java/uk/co/mruoc/cws/solver/textract/ImageProcessor.java
+++ adapters/solver-client/src/main/java/uk/co/mruoc/cws/solver/textract/ImageProcessor.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import nu.pattern.OpenCV;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -48,9 +47,7 @@ public class ImageProcessor {
   private Mat process(byte[] bytes) {
     var grid = extractGrid(bytes);
     var gray = toGrayscale(grid);
-    // var filter = filter(gray);
     return toBinary(gray);
-    // return clean(binary);
   }
 
   private Mat toOriginal(byte[] bytes) {
@@ -66,26 +63,11 @@ public class ImageProcessor {
     return gray;
   }
 
-  private Mat filter(Mat input) {
-    var filter = new Mat();
-    Imgproc.bilateralFilter(input, filter, 7, 75, 75);
-    Imgcodecs.imwrite("4-filter.png", filter);
-    return filter;
-  }
-
   private Mat toBinary(Mat input) {
     Mat binary = new Mat();
     Imgproc.adaptiveThreshold(
         input, binary, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 51, 10);
     Imgcodecs.imwrite("4-binary.png", binary);
     return binary;
-  }
-
-  private Mat clean(Mat input) {
-    Mat cleaned = new Mat();
-    Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
-    Imgproc.morphologyEx(input, cleaned, Imgproc.MORPH_OPEN, kernel);
-    Imgcodecs.imwrite("6-cleaned.png", cleaned);
-    return cleaned;
   }
 }
