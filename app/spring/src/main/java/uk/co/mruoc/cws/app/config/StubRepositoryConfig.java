254d34a1f14b8b17c46988631a830e3689ddaaf4
Michael Ruocco <michael.ruocco@hotmail.com>
2025-12-12 05:57:58 +0000

    removes unused answer repository

--- app/spring/src/main/java/uk/co/mruoc/cws/app/config/StubRepositoryConfig.java
+++ app/spring/src/main/java/uk/co/mruoc/cws/app/config/StubRepositoryConfig.java
@@ -2,10 +2,8 @@ package uk.co.mruoc.cws.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.co.mruoc.cws.repository.StubAnswerRepository;
 import uk.co.mruoc.cws.repository.StubAttemptRepository;
 import uk.co.mruoc.cws.repository.StubPuzzleRepository;
-import uk.co.mruoc.cws.usecase.AnswerRepository;
 import uk.co.mruoc.cws.usecase.attempt.AttemptRepository;
 import uk.co.mruoc.cws.usecase.puzzle.PuzzleRepository;
 
@@ -20,10 +18,5 @@ public class StubRepositoryConfig {
   @Bean
   public AttemptRepository stubAttemptRepository() {
     return new StubAttemptRepository();
-  }
-
-  @Bean
-  public AnswerRepository stubAnswerRepository() {
-    return new StubAnswerRepository();
   }
 }
