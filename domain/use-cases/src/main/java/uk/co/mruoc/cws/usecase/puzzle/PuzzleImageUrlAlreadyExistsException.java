b5f22e55dd43494c188ea027e2da0d3fdf5bf408
Michael Ruocco <michael.ruocco@hotmail.com>
2026-01-01 07:07:59 +0000

    return existing puzzle if attempt to create one that already exists rather than returning an error

--- domain/use-cases/src/main/java/uk/co/mruoc/cws/usecase/puzzle/PuzzleImageUrlAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package uk.co.mruoc.cws.usecase.puzzle;
-
-public class PuzzleImageUrlAlreadyExistsException extends RuntimeException {
-
-  public PuzzleImageUrlAlreadyExistsException(long id, String hash) {
-    super(String.format("a puzzle with id %d already exists for hash %s", id, hash));
-  }
-}
